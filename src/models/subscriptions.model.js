4d6cd760c155e9b0a9f4855d56985b6afee06e2b
Ghorasaini Success <imsaphal21@gmail.com>
2023-12-22 19:09:10 +0545

    add subscription model

---
+++ src/models/subscriptions.model.js
@@ -0,0 +1,18 @@
+import mongoose, { Schema } from "mongoose";
+
+
+const subscriptionSchema = new Schema({
+    subscriber: {
+        type: Schema.Types.ObjectId, // * one who is subscribing
+        ref: "User",
+        required: "true"
+    },
+    channel: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: 'true'
+    }
+}, { timestamps: true });
+
+
+export const Subscription = mongoose.model("Subscription", subscriptionSchema);
